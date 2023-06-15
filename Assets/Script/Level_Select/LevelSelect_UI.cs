cb34151b9d13ebeac820d97809283ad939f4f328
Aimeric <lachaimeric@gmai.com>
2023-06-15 11:25:58 -0400

    correction

--- Assets/Script/Level_Select/LevelSelect_UI.cs
+++ Assets/Script/Level_Select/LevelSelect_UI.cs
@@ -15,7 +15,7 @@ public class LevelSelect_UI : MonoBehaviour
     private int m_IndexDes;
     private int m_Lenght;
     private Coroutine m_TypeWritter;
-    private LevelsData m_data;
+    private LevelsData m_Data;
 
     private void Awake()
     {
@@ -26,33 +26,39 @@ public class LevelSelect_UI : MonoBehaviour
 
     private void PlayerOnPortal(LevelsData data)
     {
-        m_data = data;
-        m_LevelName.text = data.Name;
-        m_LevelStar.sprite = data.StarIcon[0];
-        m_IndexDes = 0;
-        m_Lenght = data.DescriptonText.Count - 1;
-        m_LevelDescription.text = "";
-        m_TypeWritter = StartCoroutine(TypeText());
-        m_LevelPath = data.SceneName;
-        m_LevelSelector.SetActive(true);
+        m_Data = data;
+        if (m_Data != null)
+        {
+            m_LevelName.text = m_Data.Name;
+            m_LevelStar.sprite = m_Data.StarIcon[0];
+            m_IndexDes = 0;
+            m_Lenght = m_Data.DescriptonText.Count - 1;
+            m_LevelDescription.text = "";
+            m_TypeWritter = StartCoroutine(TypeText());
+            m_LevelPath = m_Data.SceneName;
+            m_LevelSelector.SetActive(true);
+        }
     }
 
     private void PlayerExitPortal()
     {
         m_LevelSelector.SetActive(false);
         m_IndexDes = 0;
-        StopCoroutine(m_TypeWritter);
         m_LevelDescription.text = "";
+        if (m_TypeWritter != null)
+        {
+            StopCoroutine(m_TypeWritter);
+        }
     }
 
     private IEnumerator TypeText()
     {
-        foreach (char _char in m_data.DescriptonText[m_IndexDes].ToCharArray())
+        foreach (char _char in m_Data.DescriptonText[m_IndexDes].ToCharArray())
         {
             m_LevelDescription.text += _char;
             yield return new WaitForSeconds(0.2f);
         }
-        m_LevelDescription.text = m_data.DescriptonText[m_IndexDes];
+        m_LevelDescription.text = m_Data.DescriptonText[m_IndexDes];
 
     }
 
@@ -72,7 +78,7 @@ public class LevelSelect_UI : MonoBehaviour
         }
         else
         {
-            m_LevelDescription.text = m_data.DescriptonText[m_IndexDes];
+            m_LevelDescription.text = m_Data.DescriptonText[m_IndexDes];
         }
     }
 }
